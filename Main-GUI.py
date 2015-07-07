7da4f157289c1886b6af937d1a82ddd425c196fa
Ryan Owens <RyanOwens@linux.com>
2015-07-07 17:54:45 -0500

    sum checking and removal of unneeded bash file for copying certificate

--- Main-GUI.py
+++ Main-GUI.py
@@ -4,6 +4,58 @@ import tkMessageBox
 import dbus
 import uuid
 import os
+import shutil
+import hashlib
+
+def getFileMD5SUM(filename, blocksize=2*20):
+    md5Object = hashlib.md5()
+    #open the file for reading
+    with open(filename, 'r') as f:
+        while True:
+            #add file contents to buffer in blocksize chunks
+            buffer = f.read(blocksize)
+            #when the buffer is empty
+            if not buffer:
+                break
+            #add buffer to md5sum object
+            md5Object.update(buffer)
+    f.close()
+    #hexidigest is a string with the md5sum in hex format
+    return md5Object.hexdigest()
+
+def security_error_window(message):
+    tkMessageBox.showerror(title="Certificate Error", message=message)
+    #we also stop the program...
+    root.destroy()
+
+def isSumCorrect(file, properHash):
+    #don't assume file exists...
+    if os.path.isfile(file):
+        #TODO don't use insecure MD5 but rather sha256 or sha512
+        sum = getFileMD5SUM(file)
+        if properHash == sum:
+            return True
+        else:
+            security_error_window("Error: Certificate does not pass security check.")
+    else:
+        security_error_window("Error: Certificate is missing. There may be an issue with your download.")
+
+
+def setup_Certificate(cert_location, cert_name):
+    #Check if the directory for the certificate exists, if not make it
+    if not os.path.exists(cert_location):
+        os.mkdir(cert_location)
+    #If the certificate exists alreay, remove it to keep things pure.
+    if os.path.isfile(cert_location + cert_name):
+        os.remove(cert_location + cert_name)
+    #get the current working directory
+    work_dir = os.getcwd()
+    #check the files sha256 sum
+    #TODO get hash from file for better security IE from ITS directly via .md5sum file or .sha*sum file
+    properHash = "1663fb443486f27ae568b9da1eaf1a0a"
+    if isSumCorrect(work_dir + "/" + cert_name, properHash):
+        shutil.copy2(work_dir + "/" + cert_name, cert_location)
+
 
 def path_dbusByteArray(path):
     #Generate a Dbuse ByteArray to be used with ca-cert
@@ -11,8 +63,11 @@ def path_dbusByteArray(path):
 
 def WPASETUP():
     user_home = os.getenv("HOME")
-    cert_location = user_home + '/.config/SIUE_WPA/ca.crt'
+    cert_location = user_home + '/.config/SIUE_WPA/'
+    cert_name = "oitca.cer"
     userUUID = str(uuid.uuid4())
+
+    setup_Certificate(cert_location, cert_name)
 
     eid = entryWidget.get()
 
@@ -23,16 +78,18 @@ def WPASETUP():
 
     s_wifi = dbus.Dictionary({
         'ssid': dbus.ByteArray("SIUE-WPA"),
+        'mode': 'infrastructure',
         'security': '802-11-wireless-security'})
 
     s_wsec = dbus.Dictionary({
-        'key-mgmt': 'wpa-eap',
-        'auth-alg': 'open'})
+        'key-mgmt': 'wpa-eap'})
 
     s_8021x = dbus.Dictionary({
         'eap': ['peap'],
         'identity': eid,
         'ca-cert': path_dbusByteArray(cert_location),
+        'system-ca-certs': True,
+        'password-flags': dbus.UInt32(1),
         'phase2-auth': 'mschapv2'})
 
     s_ip4 = dbus.Dictionary({'method': 'auto'})
